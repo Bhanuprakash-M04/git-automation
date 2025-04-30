afd8b10642fbc3e9d1208127df778dd9d5042157
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-04-30 13:07:30 +0530

    Update Solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -6,10 +6,6 @@ import java.util.*;
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
 
-
-        
         //Write your code
-        // remove the below lines while running the code
-        return Collections.emptyList();
     }
 }
