3c87c2ad01321072200122ffa2c532953171c4f3
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-04-30 14:01:05 +0530

    Update solution.c

--- solutions/solution.c
+++ solutions/solution.c
@@ -3,13 +3,27 @@
 #include <stdbool.h>
 int *spiralOrder(int **matrix, int matrixSize, int *matrixColSize, int *returnSize)
 {
+    int rows = matrix.size();
+        int cols = matrix[0].size();
+        int x = 0;
+        int y = 0;
+        int dx = 1;
+        int dy = 0;
+        vector<int> res;
 
+        for (int i = 0; i < rows * cols; i++) {
+            res.push_back(matrix[y][x]);
+            matrix[y][x] = -101;
 
+            if (!(0 <= x + dx && x + dx < cols && 0 <= y + dy && y + dy < rows) || matrix[y+dy][x+dx] == -101) {
+                int temp = dx;
+                dx = -dy;
+                dy = temp;
+            }
 
+            x += dx;
+            y += dy;
+        }
 
-    
-    // Write your code here
-    // Remove the below lines while running the code 
-    *returnSize = 0;
-    return NULL;
+        return res;
 }
