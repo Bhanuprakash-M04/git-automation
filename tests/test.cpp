87b21d1858dbda89825c28a0447cb5d38656f637
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-04-30 11:58:58 +0530

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -17,7 +17,6 @@ void runTestCases()
         {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}},
         {{1, 2}, {3, 4}},
         {{1}},
-        {},
         {{1}, {2}, {3}}};
 
     vector<vector<int>> expectedResults = {
@@ -25,7 +24,6 @@ void runTestCases()
         {1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7},
         {1, 2, 4, 3},
         {1},
-        {},
         {1, 2, 3}};
 
     Solution sol;
